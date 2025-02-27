91439aa88827a1a0ff148f53394d4de8558eed13
Jorge_castro1 <jorge.castro1@catolica.edu.sv>
2025-02-27 17:21:51 -0600

    se agregaron las configuraciones de las coneccion a la DB

---
+++ P01_2022-CG-650_2022-CC-601/Models/ResevasContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace P01_2022_CG_650_2022_CC_601.Models
+{
+    public class ResevasContext : DbContext
+    {
+
+        public ResevasContext(DbContextOptions<ResevasContext> option) : base(option)
+
+        {
+
+        }
+    }
+}
--- P01_2022-CG-650_2022-CC-601/Program.cs
+++ P01_2022-CG-650_2022-CC-601/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using P01_2022_CG_650_2022_CC_601.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//Inyeccion por dependencia de la base de datos
+builder.Services.AddDbContext<ResevasContext>(options =>
+    options.UseSqlServer(
+             builder.Configuration.GetConnectionString("ReservaDbConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
