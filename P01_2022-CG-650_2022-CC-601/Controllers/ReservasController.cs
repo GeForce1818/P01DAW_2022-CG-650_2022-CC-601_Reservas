d0f2c332a820fc3ec472122213193bc0f3c2e282
Jorge_castro1 <jorge.castro1@catolica.edu.sv>
2025-02-27 20:11:40 -0600

    e

--- P01_2022-CG-650_2022-CC-601/Controllers/ReservasController.cs
+++ P01_2022-CG-650_2022-CC-601/Controllers/ReservasController.cs
@@ -19,33 +19,10 @@ namespace P01_2022_CG_650_2022_CC_601.Controllers
 
 
 
-        [HttpGet]
-        [Route("Todas las reservas")]
-        public IActionResult GetReservas()
-        {
-            var listaReservas = (from r in _ReservaContext.
-                                        
-            if (listaReservas.Count == 0)
-            {
-                return NotFound();
-            }
-            return Ok(listaReservas);
-        }
+        
 
 
-        [HttpGet]
-        [Route("Reserva Por ID/{id}")]
-        public IActionResult GetReserva(int id)
-        {
-            var reserva = (from r in _ReservaContext.
-                           where r.Id == id
-                           select r).FirstOrDefault();
-            if (reserva == null)
-            {
-                return NotFound();
-            }
-            return Ok(reserva);
-        }
+       
     }
 
 
