d789c4562679428f165070bb4561a7f0017471d3
AlexLC96 <soyunfurryowo@gmail.com>
2025-02-27 18:41:42 -0600

    adición de controladores

---
+++ P01_2022-CG-650_2022-CC-601/Controllers/sucursalController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P01_2022_CG_650_2022_CC_601.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class sucursalController : ControllerBase
+    {
+    }
+}
