18f3e60a72bba65037f017bd4bebde52b7452af7
Jorge_castro1 <jorge.castro1@catolica.edu.sv>
2025-02-27 20:30:23 -0600

    reserva controller

--- P01_2022-CG-650_2022-CC-601/Controllers/ReservasController.cs
+++ P01_2022-CG-650_2022-CC-601/Controllers/ReservasController.cs
@@ -16,13 +16,37 @@ namespace P01_2022_CG_650_2022_CC_601.Controllers
         {
             _ReservaContext = Reservacontext;
         }
+        /*
+        [HttpGet]
+        [Route("Espacios Disponibles/{fecha}/{hora}")]
+        public IActionResult GetEspaciosDisponibles(DateOnly fecha, string hora)
+        {
+            // Consulta los espacios que ya están reservados en la fecha y hora dada
+            var espaciosReservados = _ReservaContext.idespacio
+                .Where(r => r.Fecha == fecha && r.Hora == hora)
+                .Select(r => r.IdEspacio)
+                .ToList();
+
+            // Consulta los espacios disponibles (los que no están reservados)
+            var espaciosDisponibles = _ReservaContext.idespacio
+                .Where(e => !espaciosReservados.Contains(e.Id))
+                .ToList();
+
+            // Si no hay espacios disponibles, retorna un NotFound
+            if (espaciosDisponibles.Count == 0)
+            {
+                return NotFound();
+            }
+
+            // Retorna los espacios disponibles
+            return Ok(espaciosDisponibles);
+        }
+        */
+
+        // ya no se supo que hacer 
 
 
 
-        
-
-
-       
     }
 
 
